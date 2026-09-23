35cbae403b2f5312c4c5eed8919552966e7be7b2
Leo Ortiz <leo21oritz7@gmail.com>
2026-09-23 16:02:13 -0500

    Created Role class

---
+++ src/java/business/Role.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author leo21
+ */
+public class Role implements Serializable {
+    private int roleID, userID;
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(int roleID, int userID, String role) {
+        this.roleID = roleID;
+        this.userID = userID;
+        this.role = role;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+}
